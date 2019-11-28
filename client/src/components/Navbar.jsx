ce313e05d4ca5142a6b20f3be5b6224450dc420b
Erwan Le Cren <lecren.erwan@gmail.com>
2019-11-28 15:32:24 +0100

    Bars btn

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from '../assets/pictures/techCanonLogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
@@ -7,8 +9,9 @@ const Navbar = () => {
       <div class="Navbar__main-container">
         <img class="Navbar__logo" src={Logo} alt="Logo" />
         <div class="Navbar__btn-container">
-          <button className="Navbar__btn Navbar__btn-blue1">Inscription</button>
-          <button className='Navbar__btn Navbar__btn-blue2'>Connexion</button>
+          <button className="hidden Navbar__btn Navbar__btn-blue1">Inscription</button>
+          <button className='hidden Navbar__btn Navbar__btn-blue2'>Connexion</button>
+          <button className="Navbar__bars-btn" ><FontAwesomeIcon icon={faBars} /></button>
         </div>
       </div>
     </div>
