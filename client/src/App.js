af70beb894b707eb8fe657b092b41c16643fe427
Erwan Le Cren <lecren.erwan@gmail.com>
2019-11-28 12:11:22 +0100

    added sass

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import Login from './containers/Login'
 import Register from './containers/Register'
 import PostList from './containers/PostList'
 import Post from './containers/Post'
+import './styles/main.scss'
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={ Login } />
-        <Route path='/login' component={ Login } />
-        <Route path='/register' component={ Register } />
-        <Route path='/post' component={ Post } />
-        <Route path='/postList' component={ PostList } />
-      </Switch>
-    </Router>
+    <div className="TechCanon">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={ Login } />
+          <Route path='/login' component={ Login } />
+          <Route path='/register' component={ Register } />
+          <Route path='/post' component={ Post } />
+          <Route path='/postList' component={ PostList } />
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
