101719001314432c78162d629bc125e0b8c98c64
Erwan Le Cren <lecren.erwan@gmail.com>
2019-11-28 14:32:44 +0100

    Navbar v1

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Logo from '../assets/pictures/techCanonLogo.png'
 
 const Navbar = () => {
   return (
     <div class="Navbar__container">
-      
+      <div class="Navbar__main-container">
+        <img class="Navbar__logo" src={Logo} alt="Logo" />
+        <div class="Navbar__btn-container">
+          <button className="Navbar__btn Navbar__btn-blue1">Inscription</button>
+          <button className='Navbar__btn Navbar__btn-blue2'>Connexion</button>
+        </div>
+      </div>
     </div>
   )
 }
