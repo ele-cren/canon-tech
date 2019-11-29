61b48005cc72b6dafb38c5eec038e917cc2662d0
Erwan Le Cren <erwan.lecren@hotmail.com>
2019-11-29 14:15:35 +0100

    started menu navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,59 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../assets/pictures/techCanonLogo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 
 const Navbar = () => {
+  const [isMobile, setMobile] = useState(false)
   useEffect(() => {
-    //checkWindowSize()
-    //window.addEventListener('resize', checkWindowSize)
+    checkWindowSize()
+    window.addEventListener('resize', checkWindowSize)
     return () => {
-      //window.removeEventListener('resize', checkWindowSize)
+      window.removeEventListener('resize', checkWindowSize)
     }
   }, [])
 
-  const updateLoginBtns = (display = true) => {
-    [...document.getElementsByClassName('Navbar__btn')].forEach((btn) => {
-      if (!btn.classList.contains('hidden') && !display) {
-        btn.classList.add('hidden')
-      } else if (btn.classList.contains('hidden') && display) {
-        btn.classList.remove('hidden')
-      }
-    })
-  }
-
-  const updateBarsBtn = (display = true) => {
-    [...document.getElementsByClassName('Navbar__bars-btn')].forEach((btn) => {
-      if (!btn.classList.contains('hidden') && !display) {
-        btn.classList.add('hidden')
-      } else if (btn.classList.contains('hidden') && display) {
-        btn.classList.remove('hidden')
-      }
-    })
-  }
-
   const checkWindowSize = () => {
     if (window.innerWidth <= 465) {
-      updateLoginBtns(false)
-      updateBarsBtn(true)
+      setMobile(true)
     } else {
-      updateBarsBtn(false)
-      updateLoginBtns(true)
+      setMobile(false)
     }
   }
 
   return (
-    <div className="Navbar__container">
-      <div className="Navbar__main-container">
-        <img className="Navbar__logo" src={Logo} alt="Logo" />
-        <nav className="Navbar__btn-container">
-          <Link to="/register" className="hidden Navbar__btn Navbar__btn-blue1">Inscription</Link>
-          <Link to="/login" className='hidden Navbar__btn Navbar__btn-blue2'>Connexion</Link>
-          <button className="Navbar__bars-btn" ><FontAwesomeIcon icon={faBars} /></button>
-        </nav>
+    <>
+      <div className="Navbar__container">
+        <div className="Navbar__main-container">
+          <img className="Navbar__logo" src={Logo} alt="Logo" />
+          <nav className="Navbar__btn-container">
+            <Link to="/register" className={classNames('Navbar__btn', 'Navbar__btn-blue1', { hidden: isMobile })}>Inscription</Link>
+            <Link to="/login" className={classNames('Navbar__btn', 'Navbar__btn-blue2', { hidden: isMobile })}>Connexion</Link>
+            <button className={classNames('Navbar__bars-btn', { hidden: !isMobile })}><FontAwesomeIcon icon={faBars} /></button>
+          </nav>
+        </div>
       </div>
-    </div>
+      <nav className="Navbar__menu-mobile">
+        <ul>
+          <li>Inscription</li>
+          <li>Connexion</li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
