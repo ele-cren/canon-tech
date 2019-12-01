9ac5513d48317b911e0caff606b28f7f5f80617a
Erwan <lecren.erwan@gmail.com>
2019-12-01 17:57:37 +0100

    Navbar integration ok

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,20 +7,34 @@ import classNames from 'classnames'
 
 const Navbar = () => {
   const [isMobile, setMobile] = useState(false)
+  const [isMenuOpened, setMenu] = useState(false)
+  const [width, setWidth] = useState(0)
+  
   useEffect(() => {
-    checkWindowSize()
-    window.addEventListener('resize', checkWindowSize)
+    updateWidth()
+    window.addEventListener('resize', () => updateWidth())
     return () => {
-      window.removeEventListener('resize', checkWindowSize)
+      window.removeEventListener('resize', () => updateWidth())
     }
   }, [])
 
-  const checkWindowSize = () => {
-    if (window.innerWidth <= 465) {
-      setMobile(true)
+  useEffect(() => {
+    if (width <= 465) {
+      if (!isMobile) {
+        setMobile(true)
+      }
     } else {
-      setMobile(false)
+      if (isMobile) {
+        setMobile(false)
+      }
+      if (isMenuOpened) {
+        setMenu(false)
+      }
     }
+  })
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth)
   }
 
   return (
@@ -31,14 +45,14 @@ const Navbar = () => {
           <nav className="Navbar__btn-container">
             <Link to="/register" className={classNames('Navbar__btn', 'Navbar__btn-blue1', { hidden: isMobile })}>Inscription</Link>
             <Link to="/login" className={classNames('Navbar__btn', 'Navbar__btn-blue2', { hidden: isMobile })}>Connexion</Link>
-            <button className={classNames('Navbar__bars-btn', { hidden: !isMobile })}><FontAwesomeIcon icon={faBars} /></button>
+            <button onClick={() => setMenu(!isMenuOpened)} className={classNames('Navbar__bars-btn', { hidden: !isMobile })}><FontAwesomeIcon icon={faBars} /></button>
           </nav>
         </div>
       </div>
-      <nav className="Navbar__menu-mobile">
+      <nav className={classNames('Navbar__menu-mobile', { hidden: !isMenuOpened })}>
         <ul>
-          <li>Inscription</li>
-          <li>Connexion</li>
+          <li><Link onClick={() => setMenu(false)} className="Navbar__menu-link" to="/register">Inscription</Link></li>
+          <li><Link onClick={() => setMenu(false)} className="Navbar__menu-link" to="/login">Connexion</Link></li>
         </ul>
       </nav>
     </>
