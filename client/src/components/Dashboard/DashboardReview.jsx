62c5a2ee470e782057ef340d15d5346118e0e5c6
Erwan Le Cren <erwan.lecren@hotmail.com>
2019-12-26 13:05:19 +0100

    reviews done

--- client/src/components/Dashboard/DashboardReview.jsx
+++ client/src/components/Dashboard/DashboardReview.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 const DashboardReview = (props) => {
   return (
-    <div className="DashboardReview__container">
-      <img src={props.img} alt="Image" className="DashboardReview__img"/>
-      <div className="DashboardReview__review">
-
+    <>
+      <div className="DashboardReview__container">
+        <img src={props.img} alt="Image" className="DashboardReview__img"/>
+        <div className="DashboardReview__review">
+          <div className="DashboardReview__title">{props.title}</div>
+          {props.review}
+          <div className="DashboardReview__overflow-handler">Lire</div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- client/src/containers/Dashboard/DashboardReviews.jsx
+++ client/src/containers/Dashboard/DashboardReviews.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import DashboardReview from '../../components/Dashboard/DashboardReview'
 
 const DashboardReviews = () => {
+  let reviewText = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. N'
   let reviews = []
   for (let i = 0; i < 3; i++) {
-    reviews = [...reviews, <DashboardReview key={i} img="https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Blade_Runner_%281982_poster%29.png/220px-Blade_Runner_%281982_poster%29.png" />]
+    reviews = [...reviews, <DashboardReview key={i}
+      title={'Blade Runner'} review={reviewText}
+      img="https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Blade_Runner_%281982_poster%29.png/220px-Blade_Runner_%281982_poster%29.png" />]
   }
   return (
     <div className="DashboardReviews__container">
